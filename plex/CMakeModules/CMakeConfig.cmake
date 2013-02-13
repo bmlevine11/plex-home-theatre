73596fb8b2bb5a82b9bb3da63c80c4a9181958ba
Dale Hamel <dale.hamel@srvthe.net>
2013-02-13 06:57:46 -0600

    Disabled DVD drive support, don't think the pi has one ;)

--- plex/CMakeModules/CMakeConfig.cmake
+++ plex/CMakeModules/CMakeConfig.cmake
@@ -10,7 +10,7 @@ set(CONFIG_INTERNAL_LIBS
   lib_upnp
 )
 
-OPTION(ENABLE_DVD_DRIVE "Enable the DVD drive" ON)
+#OPTION(ENABLE_DVD_DRIVE "Enable the DVD drive" ON)
 
 if(APPLE)
   include(CMakeConfigOSX)
