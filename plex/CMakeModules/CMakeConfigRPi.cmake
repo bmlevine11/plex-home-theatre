25164f50060f492e3075ff366252c3395c22a651
Robert Buhren <robert@robertbuhren.de>
2013-02-13 17:27:58 +0100

    Remove GLEW and SDL dependencies Add initial support for cross-compiling with openelec toolchain

--- plex/CMakeModules/CMakeConfigRPi.cmake
+++ plex/CMakeModules/CMakeConfigRPi.cmake
@@ -1,5 +1,3 @@
-# vim: setlocal syntax=cmake:
-
 if(UNIX)
   set(CMAKE_REQUIRED_FLAGS "-D__LINUX_USER__")
 endif()
@@ -11,8 +9,6 @@ set(LINK_PKG
   JPEG
   SQLite3
   PCRE
-  SDL
-  SDL_image
   Lzo2
   FriBiDi
   Fontconfig
@@ -21,29 +17,16 @@ set(LINK_PKG
   microhttpd
   Crypto
   TinyXML
-  GLEW
   Iconv
   Avahi
   LibDl
   LibRt
 )
 
-
-
-#        --disable-optical-drive 
-#  --disable-debug \
-#   \
-# --with-platform=raspberry-pi --enable-optimizations \
-#                    --enable-libcec --enable-player=omxplayer
-
 foreach(l ${LINK_PKG})
   plex_find_package(${l} 1 1)
 endforeach()
 
-#find_package(OpenGLES2 REQUIRED)
-#include_directories(${OpenGLES2_INCLUDE_DIRS})
-#set(CONFIG_PLEX_LINK_LIBRARIES ${CONFIG_PLEX_LINK_LIBRARIES} ${OpenGLES2_LIBRARIES})
-#  
 find_package(Boost COMPONENTS thread system REQUIRED)
 if(Boost_FOUND)
   include_directories(${Boost_INCLUDE_DIRS})
@@ -138,31 +121,21 @@ add_definitions(
     -DNDEBUG=1 
 )
 
-include_directories(
-    /opt/vc/include/ 
-    /opt/vc/include/EGL 
-   # /opt/vc/include/GLES 
-    /opt/vc/include/GLES2 
-    /opt/vc/include/KHR 
-    /opt/vc/include/VG 
-    /opt/vc/include/WF 
-    /opt/vc/include/vc/include  
-)
+
+plex_find_library(GLESv2 0  0 system/opt/vc/lib 1)
+plex_find_library(EGL 0 0  system/opt/vc/lib 1)
+plex_find_library(vcos 0 0  system/opt/vc/lib 1)
+plex_find_library(bcm_host 0 0  system/opt/vc/lib 1)
+plex_find_library(vchiq_arm 0 0  system/opt/vc/lib 1)
 
 
 
+#needed for the commandline flag CMAKE_INCLUDE_PATH
+foreach(path ${CMAKE_INCLUDE_PATH})
+  include_directories(${path})
+endforeach()
 
 
-plex_find_library(GLESv2 0  0 /opt/vc/lib 1)
-plex_find_library(EGL 0 0  /opt/vc/lib 1)
-plex_find_library(vcos 0 0  /opt/vc/lib 1)
-plex_find_library(bcm_host 0 0  /opt/vc/lib 1)
-plex_find_library(vchiq_arm 0 0  /opt/vc/lib 1)
-
-
-set(CMAKE_C_FLAGS " -isystem/usr/include -isystem/opt/vc/include -isystem/opt/vc/include/interface/vcos/pthreads -isystem/opt/vc -isystem/opt/vc/include/interface/vmcs_host/linux/ -isystem/opt/vc/include/EGL -isystem/opt/vc/include/GLES -isystem/opt/vc/include/GLES2 -isystem/opt/vc/include/KHR -isystem/opt/vc/include/VG -L/lib -L/usr/lib -L/opt/vc/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/lib -Wl,-rpath-link,/usr/lib -Wl,-rpath-link,/opt/vc/ -fPIC -pipe -O3 -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp -mabi=aapcs-linux -Wno-psabi -Wa,-mno-warn-deprecated -Wno-deprecated-declarations ")
-
-set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
 
 set(LIBPATH bin)
 set(BINPATH bin)
@@ -170,7 +143,7 @@ set(RESOURCEPATH share/XBMC)
 
 set(PLEX_LINK_WRAPPED "-Wl,--unresolved-symbols=ignore-all -Wl,-wrap,_IO_getc -Wl,-wrap,_IO_getc_unlocked -Wl,-wrap,_IO_putc -Wl,-wrap,__fgets_chk -Wl,-wrap,__fprintf_chk -Wl,-wrap,__fread_chk -Wl,-wrap,__fxstat64 -Wl,-wrap,__lxstat64 -Wl,-wrap,__printf_chk -Wl,-wrap,__read_chk -Wl,-wrap,__vfprintf_chk -Wl,-wrap,__xstat64 -Wl,-wrap,_stat -Wl,-wrap,calloc -Wl,-wrap,clearerr -Wl,-wrap,close -Wl,-wrap,closedir -Wl,-wrap,dlopen -Wl,-wrap,fclose -Wl,-wrap,fdopen -Wl,-wrap,feof -Wl,-wrap,ferror -Wl,-wrap,fflush -Wl,-wrap,fgetc -Wl,-wrap,fgetpos -Wl,-wrap,fgetpos64 -Wl,-wrap,fgets -Wl,-wrap,fileno -Wl,-wrap,flockfile -Wl,-wrap,fopen -Wl,-wrap,fopen64 -Wl,-wrap,fprintf -Wl,-wrap,fputc -Wl,-wrap,fputs -Wl,-wrap,fread -Wl,-wrap,free -Wl,-wrap,freopen -Wl,-wrap,fseek -Wl,-wrap,fseeko64 -Wl,-wrap,fsetpos -Wl,-wrap,fsetpos64 -Wl,-wrap,fstat -Wl,-wrap,ftell -Wl,-wrap,ftello64 -Wl,-wrap,ftrylockfile -Wl,-wrap,funlockfile -Wl,-wrap,fwrite -Wl,-wrap,getc -Wl,-wrap,getc_unlocked -Wl,-wrap,getmntent -Wl,-wrap,ioctl -Wl,-wrap,lseek -Wl,-wrap,lseek64 -Wl,-wrap,malloc -Wl,-wrap,open -Wl,-wrap,open64 -Wl,-wrap,opendir -Wl,-wrap,popen -Wl,-wrap,printf -Wl,-wrap,read -Wl,-wrap,readdir -Wl,-wrap,readdir64 -Wl,-wrap,realloc -Wl,-wrap,rewind -Wl,-wrap,rewinddir -Wl,-wrap,setvbuf -Wl,-wrap,ungetc -Wl,-wrap,vfprintf -Wl,-wrap,write")
 
-set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /opt/vc/lib )
+set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} system/opt/vc/lib )
 
 
 set(PLEX_LINK_WHOLEARCHIVE -Wl,--whole-archive)
