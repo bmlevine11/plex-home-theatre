fe6c73fd42c4a244a5ab01ffcd6cd6b9d90c26d0
LongChair <LongChair@hotmail.com>
2014-04-08 13:21:39 +0200

    [MERGE] Reverting shutdown messages tweaks as shutdown is corrected
    Suspend and hibernate will reboot, but it's no big deal. Should we want this to be fixed properly, we should hide the shutdown / hibernate menu items within the sking stuff as its not supported.

--- xbmc/ApplicationMessenger.cpp
+++ xbmc/ApplicationMessenger.cpp
@@ -224,16 +224,10 @@ void CApplicationMessenger::ProcessMessage(ThreadMessage *pMsg)
       {
         switch (g_guiSettings.GetInt("powermanagement.shutdownstate"))
         {
-
-          #if TARGET_RASPBERRY_PI
-          case POWERSTATE_SUSPEND:
-          case POWERSTATE_HIBERNATE:
-          #endif
           case POWERSTATE_SHUTDOWN:
             Powerdown();
             break;
 
-          #ifndef TARGET_RASPBERRY_PI
           case POWERSTATE_SUSPEND:
             Suspend();
             break;
@@ -241,7 +235,6 @@ void CApplicationMessenger::ProcessMessage(ThreadMessage *pMsg)
           case POWERSTATE_HIBERNATE:
             Hibernate();
             break;
-          #endif
 
           case POWERSTATE_QUIT:
             Quit();
@@ -258,10 +251,6 @@ void CApplicationMessenger::ProcessMessage(ThreadMessage *pMsg)
       }
       break;
 
-#if TARGET_RASPBERRY_PI
-    case TMSG_SUSPEND:
-    case TMSG_HIBERNATE:
-#endif
     case TMSG_POWERDOWN:
       {
         g_application.Stop(EXITCODE_POWERDOWN);
@@ -275,7 +264,6 @@ void CApplicationMessenger::ProcessMessage(ThreadMessage *pMsg)
       }
       break;
 
-#ifndef TARGET_RASPBERRY_PI
     case TMSG_HIBERNATE:
       {
         g_PVRManager.SetWakeupCommand();
@@ -289,7 +277,6 @@ void CApplicationMessenger::ProcessMessage(ThreadMessage *pMsg)
         g_powerManager.Suspend();
       }
       break;
-#endif
 
     case TMSG_RESTART:
     case TMSG_RESET:
