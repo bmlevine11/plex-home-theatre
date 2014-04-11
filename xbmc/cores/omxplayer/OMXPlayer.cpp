7179e4cbc1cf3b7cbdb761971525542c4b4da92d
Lionel CHAZALLON <LongChair@hotmail.com>
2014-04-11 13:04:20 +0200

    [FIX] Apply upstream DVDPlayer patch to OMXPlayer

--- xbmc/cores/omxplayer/OMXPlayer.cpp
+++ xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -2495,12 +2495,13 @@ void COMXPlayer::HandleMessages()
         if (speed != DVD_PLAYSPEED_PAUSE && m_playSpeed != DVD_PLAYSPEED_PAUSE && speed != m_playSpeed)
           m_callback.OnPlayBackSpeedChanged(speed / DVD_PLAYSPEED_NORMAL);
 
+#ifndef __PLEX__
         if (m_pInputStream->IsStreamType(DVDSTREAM_TYPE_PVRMANAGER) && speed != m_playSpeed)
         {
           CDVDInputStreamPVRManager* pvrinputstream = static_cast<CDVDInputStreamPVRManager*>(m_pInputStream);
           pvrinputstream->Pause( speed == 0 );
         }
-
+#endif
         // if playspeed is different then DVD_PLAYSPEED_NORMAL or DVD_PLAYSPEED_PAUSE
         // audioplayer, stops outputing audio to audiorender, but still tries to
         // sleep an correct amount for each packet
